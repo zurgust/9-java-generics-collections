79b954ea690e3e4011519e0e8c84ff87ad56acfe
Zakharov Danila <danila.zaxarov.2015@gmail.com>
2024-11-29 07:59:19 +0300

    task02

--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,70 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
-public class SavedList<E extends Serializable> extends AbstractList<E> {
+public class SavedList<E extends List> extends AbstractList<E> {
+    private final List<E> list;
+    private final File file;
 
     public SavedList(File file) {
+        this.file = file;
+        this.list = loadFromFile();
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return list.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E oldValue = list.set(index, element);
+        saveToFile();
+        return oldValue;
     }
 
     @Override
     public int size() {
-        return 0;
+        return list.size();
     }
 
     @Override
     public void add(int index, E element) {
+        list.add(index, element);
+        saveToFile();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E removedElement = list.remove(index);
+        saveToFile();
+        return removedElement;
+    }
+
+    private void saveToFile() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
+            oos.writeObject(list);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Метод для загрузки списка из файла
+    @SuppressWarnings("unchecked")
+    private List<E> loadFromFile() {
+        if (!file.exists()) {
+            return new ArrayList<>();
+        }
+
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+            return (List<E>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
     }
 }
